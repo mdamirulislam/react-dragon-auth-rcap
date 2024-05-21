2bdfb73e5361d0781e96ff14c22d03ff7ada5304
mdamirulislam <mdamirulislam.prime@gmail.com>
2024-05-21 18:33:13 +0600

    first commit

---
+++ src/pages/Home/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h1>This is login bro</h1>
+        </div>
+    );
+};
+
+export default Login;
